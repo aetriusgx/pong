287dcc50882c3000372d3ac07d52ac47753689e9
kovuko <jason.eh20@gmail.com>
2020-07-09 11:04:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function setup(){
+const canvas = {
+  height: 600,
+  width: 600
+};
 
+function setup(){
+  createCanvas(canvas.width, canvas.height);
 }
 
 function draw(){
-
+  background(0);
 }
