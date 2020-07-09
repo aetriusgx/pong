37cbb3e09cb51b4674efe0694d0b855c24199e27
kovuko <jason.eh20@gmail.com>
2020-07-09 11:52:35 -0700

    commit 4

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
+const colours = {
+  background: 0
+}
+
 const canvas = {
-  height: 600,
-  width: 600
+  height: 1000,
+  width: 1500
+};
+
+var bracket = {
+  left: {
+    x: canvas.width * 0.05,
+    y: canvas.height * 0.25,
+    width: canvas.width * 0.025,
+    height: canvas.height * 0.25
+  },
+  right: {
+    x: canvas.width * 0.925,
+    y: canvas.height * 0.25,
+    width: canvas.width * 0.025,
+    height: canvas.height * 0.5
+  }
 };
 
 function setup(){
   createCanvas(canvas.width, canvas.height);
+  colorMode(HSB, 365, 100, 100);
 }
 
 function draw(){
-  background(0);
+  background(colours.background);
+  moveOnSides();
+
+
+  //Left side bracket
+  rect(bracket.left.x, bracket.left.y, bracket.left.width, bracket.left.height);
+  //Right side bracket
+  rect(bracket.right.x, bracket.right.y, bracket.right.width, bracket.right.height);
+  //Dividing line
+  stroke(100);
+  line(canvas.width * 0.5, 0, canvas.width * 0.5, canvas.height);
+}
+
+function moveOnSides(){
+  if(mouseX > 0 && mouseX < canvas.width * 0.5){
+    if(mouseY <= 0){
+      bracket.left.y = 0;
+    }
+    else if(mouseY >= canvas.height - bracket.left.height){
+      bracket.left.y = canvas.height - bracket.left.height;
+    } else {
+      bracket.left.y = mouseY;
+    }
+  }
 }
